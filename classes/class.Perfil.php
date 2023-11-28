d720c1d8bce8403d2de2adad7f735b27dd943a1b
felipe facury <felipe-facury@hotmail.com>
2023-11-28 16:02:22 -0300

    alteração das classes de perfil, apenas uma classe para isso

--- classes/class.Perfil.php
+++ classes/class.Perfil.php
@@ -4,6 +4,12 @@ include_once __DIR__ . '/../global.php';
 
 abstract class Perfil {
 
-  public function __construct() {}
+  private string $_nome;
+  private        $_permissoes = [];
+
+  public function __construct(string $nome, $permissoes) {
+    $this->_nome = $nome;
+    $this->_permissoes = $permissoes;
+  }
   
 }
